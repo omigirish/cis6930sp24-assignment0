e06a56ed9e1e6ccce8261d7cb3fda4e5594a67ab
Girish Salunke <girishsalunke@Girishs-MacBook-Air.local>
2024-02-04 12:06:44 -0500

    URL print

--- assignment0/main.py
+++ assignment0/main.py
@@ -9,7 +9,7 @@ from assignment0.fetchincidents import fetch_incidents
 
 
 def main(url):
-    print(url)
+    print(url.split("/")[-1])
     """
     Calling the main function should download data insert it into a database and print a summary of the incidents.
     """
